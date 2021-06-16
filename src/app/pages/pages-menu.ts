55023c7ea077ca4ca08333230f3951a73968edb8
Albert Koskei <albertkoskei@Alberts-MacBook-Pro.local>
2021-06-16 08:16:19 +0300

    first commit

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,20 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Vacancies',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'My Profile',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'My Applications',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+];
